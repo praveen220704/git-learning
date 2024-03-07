f10462d905ca663b531645fd83fe6500f9f6f709
praveen220704 <praveenprasath499@gmail.com>
2024-03-07 20:32:49 +0530

    obesitybannertask

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all")
+request.open("GET", "https://restcountries.com/v3.1/all")
+request.send();
+request.onload = function () {
+    var res = JSON.parse(request.response);
+    console.log(res);
+
+//print all 250 countries common name in console
+for (var i = 0; i < res.length; i++) {
+    console.log(res[i].name.common)
+}
+}
